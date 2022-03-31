50798303d04fb06694efe06964716c5cbf6b97f0
Jeffrey Evans <jeffreyevans.qcec@aol.com>
2022-03-31 16:35:05 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hello-world-angular';
+  title = 'My First';
+  maxOn: boolean = true;
+  names: string[] = [ "Ann", "Bob", "Chris", "David", "Ellen" ]; 
+  customers: any[] = [
+    { id: 1, name: "MAX", sales: 10000, sc: "OH" },
+    { id: 2, name: "PG", sales: 100000, sc: "LA" },
+    { id: 3, name: "Amazon", sales: 250000, sc: "IL" }
+  ];
+
+  aString1: string = "";
+  aString2: string = "";
+
+  copy():void {
+    this.aString2 = this.aString1;
+  }
+  
+
+  click(): void {
+    if(this.maxOn) {
+      this.title = "Jeffrey's";
+      this.maxOn = !this.maxOn;
+    } else {
+      this.maxOn = !this.maxOn;
+      this.title = "This is my";
+    }
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
